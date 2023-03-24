16ccda381fce76e1c9b7a5c92fe56610b2198329
CzerwinskiDawid <czerwinski.dawidd@gmail.com>
2023-03-24 23:14:38 +0100

    first commit

---
+++ Selenium/src/test/java/TestTest.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.WebDriver;
+
+public class TestTest {
+
+    private WebDriver driver;
+
+    public TestTest(WebDriver driver) {
+        this.driver = driver;
+    }
+}
