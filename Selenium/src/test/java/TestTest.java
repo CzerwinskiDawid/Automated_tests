a236c054615ffcff716ad16e958bfca6c2acfed2
CzerwinskiDawid <czerwinski.dawidd@gmail.com>
2023-03-28 12:45:02 +0200

    fast performance test

--- Selenium/src/test/java/TestTest.java
+++ Selenium/src/test/java/TestTest.java
@@ -1,12 +1,39 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
 
 public class TestTest {
 
     private WebDriver driver;
 
-    public TestTest(WebDriver driver) {
-        this.driver = driver;
+
+    @Before
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://duckduckgo.pl");
     }
 
-    //test
+    @Test
+    public void test() {
+        WebElement element = driver.findElement(By.name("q"));
+        element.sendKeys("youtube");
+        element.submit();
+    }
+
+    @After
+    public void cleanUp() {
+        driver.quit();
+    }
 }
